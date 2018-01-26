dfbdcc31c9c120e1d61cf1805aaa95341e446ba9
braincap <contactjaitiwari@gmail.com>
2018-01-26 00:11:58 -0500

    Small change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express'); //this is commonJS module. ES2015 modules us
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ bye: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000;
